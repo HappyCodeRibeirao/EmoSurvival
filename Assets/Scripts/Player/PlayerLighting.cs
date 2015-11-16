ceb2d713c1b75a917640d016dee08eb54f663fba
foresthandford <Forest.Handford@Affectiva.com>
2015-11-16 18:28:30 -0500

    AFDEXUNITY-76: Lighting increases with player fear

---
+++ Assets/Scripts/Player/PlayerLighting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLighting : MonoBehaviour {
+    Transform player;
+    public Light light;
+
+    PlayerEmotions playerEmotions;
+
+    void Start ()
+    {
+        light = GetComponent<Light>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerEmotions = player.GetComponent<PlayerEmotions>();
+    }
+	
+	void Update ()
+    {
+        if (playerEmotions != null)
+            light.range = playerEmotions.currentValence;
+    }
+}
